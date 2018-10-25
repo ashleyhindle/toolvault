1e84859d6cef64fb6232d70b82dbeb86580727b3
Ashley Hindle <ashley.hindle@siftware.com>
2018-10-25 17:41:42 +0100

    Improve bas64 and url encode/decode

--- routes/tools/base64.php
+++ routes/tools/base64.php
@@ -6,9 +6,11 @@ Route::get('/', function() {
 });
 
 Route::get('/decode/{data}', function($data) {
+    $data = preg_replace('#^/decode/#', '', $_SERVER['REQUEST_URI']);
     return response(base64_decode($data));
-});
+})->where('url', '(.*)');
 
 Route::get('/encode/{data}', function($data) {
+    $data = preg_replace('#^/encode/#', '', $_SERVER['REQUEST_URI']);
     return response(base64_encode($data));
 })->where('data', '(.*)');
--- routes/tools/url.php
+++ routes/tools/url.php
@@ -6,13 +6,17 @@ Route::get('/', function() {
 });
 
 Route::get('/parse/{url}', function($url) {
+    // Doing this allows us to take special chars (?, #, &, etc..) in the query string and not have those things be missed/parsed/removed
+    $url = preg_replace('#^/parse/#', '', $_SERVER['REQUEST_URI']);
     return response(parse_url($url));
 })->where('url', '(.*)');
 
 Route::get('/decode/{data}', function($data) {
+    $data = preg_replace('#^/decode/#', '', $_SERVER['REQUEST_URI']);
     return response(urldecode($data));
-});
+})->where('data', '(.*)');
 
 Route::get('/encode/{data}', function($data) {
+    $data = preg_replace('#^/encode/#', '', $_SERVER['REQUEST_URI']);
     return response(urlencode($data));
-});
+})->where('data', '(.*)');
