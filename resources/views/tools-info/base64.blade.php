8740765bc59de93f2a63993254e389c482c930c1
Ashley Hindle <ashley.hindle@siftware.com>
2018-10-25 17:34:37 +0100

    Add complaint about special chars in base64/url encode/decode/parse

--- resources/views/tools-info/base64.blade.php
+++ resources/views/tools-info/base64.blade.php
@@ -3,6 +3,10 @@
 
 @section('info')
     https://base64.{{ env('APP_DOMAIN') }}/encode/test
-
+    <hr />
     https://base64.{{ env('APP_DOMAIN') }}/decode/dG9vbHZhdWx0
+    <hr />
+    Problem: Passing ? or # etcetra as part of the parameter to encode/decode/parse will get missed as they think it's part of the URL, not the parameter being encoded/decoded/parsed.
+
+    For more complicated encoding/decoding/parsing we'll need to use POST
 @endsection
--- resources/views/tools-info/url.blade.php
+++ resources/views/tools-info/url.blade.php
@@ -7,5 +7,11 @@
     https://url.{{ env('APP_DOMAIN') }}/decode/dG9vbHZhdWx0
     <hr />
     URL Encode the URL first
-    https://url.{{ env('APP_DOMAIN') }}/parse/https://www.google.com/?query=string#fragment
+    https://url.{{ env('APP_DOMAIN') }}/parse/https://boris:johnson@www.google.com/?query=string#fragment
+
+    <hr />
+
+    Problem: Passing ? or # etcetra as part of the parameter to encode/decode/parse will get missed as they think it's part of the URL, not the parameter being encoded/decoded/parsed.
+
+    For more complicated encoding/decoding/parsing we'll need to use POST
 @endsection
